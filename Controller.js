5bbf3e1d72cdb9bb7b223bf6bfbc2d185f4a3c7a
Kenneth Baylosis <kenneth.baylosis@gmail.com>
2014-07-04 15:56:20 +0800

    Fix inability to recognize a mongodb Array type

--- Controller.js
+++ Controller.js
@@ -15,13 +15,13 @@ function swaggerTypeFor (type) {
   if (type === mongoose.Schema.Types.ObjectId) return 'string';
   if (type === mongoose.Schema.Types.Oid) return 'string';
   if (type === mongoose.Schema.Types.Array) return 'Array';
-  if (Array.isArray(type)) return 'Array';
+  if (Array.isArray(type) || type.name === "Array") return 'Array';
   if (type === Object) return null;
   if (type instanceof Object) return null;
   if (type === mongoose.Schema.Types.Mixed) return null;
   if (type === mongoose.Schema.Types.Buffer) return null;
   throw new Error('Unrecognized type: ' + type);
-};
+}
 
 // A method for capitalizing the first letter of a string
 function capitalize (s) {
