7ee10d375789954e49c5d254fe2594e2e7b3a6f0
Trent Edwards <tedwards@knowlysis.com>
2014-12-10 14:55:39 -0700

    Update to set the API base location path

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require("express");
 var url = require("url");
 var deco = require("deco");
 
-var Swagger = function(baucis, path) {
+var Swagger = function(baucis) {
 	this.__baucis = baucis;
-	this.basePath = path ? path : '/api';
 	var controllers = this.__controllers = [];
 	var decorators = deco.require(__dirname, ["Controller"]).hash;
 	baucis.Controller.decorators([decorators.Controller, function () {
@@ -14,39 +13,40 @@ var Swagger = function(baucis, path) {
 
 // Figure out the basePath for Swagger API definition
 function getBase (request, extra) {
-  var parts = request.originalUrl.split('/');
-  // Remove extra path parts.
-  parts.splice(-extra, extra);
-  return request.protocol + '://' + request.headers.host + parts.join('/');
+	var parts = request.originalUrl.split('/');
+	// Remove extra path parts.
+	parts.splice(-extra, extra);
+	return request.protocol + '://' + request.headers.host + parts.join('/');
 }
 
 // A method for generating a Swagger resource listing
 function generateResourceListing (options) {
-  var plurals = options.controllers.map(function (controller) {
-    return controller.model().plural();
-  });
-  var listing = {
-    apiVersion: options.version,
-    swaggerVersion: '1.1',
-    basePath: options.basePath,
-    apis: plurals.map(function (plural) {
-      return { path: '/api-docs/' + plural, description: 'Operations about ' + plural + '.' };
-    })
-  };
+	var plurals = options.controllers.map(function (controller) {
+		return controller.model().plural();
+	});
+	var listing = {
+		apiVersion: options.version,
+		swaggerVersion: '1.1',
+		basePath: options.basePath,
+		apis: plurals.map(function (plural) {
+			return { path: '/api-docs/' + plural, description: 'Operations about ' + plural + '.' };
+		})
+	};
 
-  return listing;
+	return listing;
 }
 
-Swagger.prototype.finalize = function(app) {
+Swagger.prototype.finalize = function(app, path) {
 	var controllers = this.__controllers;
+	var baseApiPath = path ? path : '/api';
 
 	controllers.forEach(function(controller) {
 		// Add routes for the controller's Swagger API definitions.
 		var route = controller.model().plural();
-	 
+
 		controller.finalize();
 
-		app.use(this.basePath + '/api-docs/' + route, function (request, response, next) {
+		app.use(baseApiPath + '/api-docs/' + route, function (request, response, next) {
 			response.set('X-Powered-By', 'Baucis');
 			response.json(deco.merge(controller.swagger, {
 				apiVersion: controller.versions(),
@@ -58,7 +58,7 @@ Swagger.prototype.finalize = function(app) {
 	});
 
 	// Activate Swagger resource listing.
-	app.use(this.basePath + '/api-docs', function (request, response, next) {
+	app.use(baseApiPath + '/api-docs', function (request, response, next) {
 		response.set('X-Powered-By', 'Baucis');
 		response.json(generateResourceListing({
 			version: "0.0.1",
